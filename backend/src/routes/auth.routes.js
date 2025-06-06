7c1faf27966bbdea178d9d86b3db41a57c385121
Ganesh <pitanibalajisatyaganesh@gmail.com>
2025-06-06 17:37:59 +0530

    damn

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -10,7 +10,7 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 router.get('/googleOauth',passport.authenticate('google', { scope: ['profile', 'email'] }));
-router.get('/googleOauth/callback',passport.authenticate('google', { session: false, failureRedirect: 'http://localhost:5173/login' }),OauthPass);
+router.get('/googleOauth/callback',passport.authenticate('google', { session: false, failureRedirect: `${process.env.GOOGLE_CALLBACK_URL}/login` }),OauthPass);
 
 router.put("/update-profile", protectRoute, updateProfile);
 
